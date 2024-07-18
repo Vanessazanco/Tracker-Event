d0ad832de1de2b7450da3c164d46e9ffd0d82ae8
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-18 15:29:49 +0100

    Hooks eventos

--- src/components/Evento/index.tsx
+++ src/components/Evento/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { IEvento } from '../../interfaces/IEvento'
 import style from './Evento.module.scss';
 import EventoCheckbox from './EventoCheckbox';
-import { useSetRecoilState } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
 import useDeletarEvento from '../../state/hooks/useDeletarEvento';
 
 const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
