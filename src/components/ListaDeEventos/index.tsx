bf709ed8d073cfdfa3f512cd5515df8684648b8d
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-19 11:27:09 +0100

    Filtros

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -3,26 +3,14 @@ import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 import useListaDeEventos from '../../state/hooks/useListaDeEventos';
-import { useRecoilValue } from 'recoil';
-import { IFiltrosDeEventos } from '../../interfaces/IFiltrosDeEventos';
-import { filtroDeEventos } from '../../state/atom';
+
 
 const ListaDeEventos: React.FC = () => {
 
-  const todosOsEventos = useListaDeEventos();
-  const filtro = useRecoilValue<IFiltrosDeEventos>(filtroDeEventos)
-
-  const eventos = todosOsEventos.filter(evento => {
-    if (!filtro.data) {
-      return true
-    }
-    const mesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-    return mesmoDia
-  }
-  )
+  const eventos = useListaDeEventos();
 
   return (<section>
-    <Filtro />
+    <Filtro/>
     <div className={style.Scroll}>
       {eventos.map(evento => (
         <Evento evento={evento} key={evento.id} />
