319a1eee7d5ff4268fc90f6e5b9342dcf2a20060
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-19 10:42:56 +0100

    Atom

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,8 +1,14 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
+import { IFiltrosDeEventos } from "../interfaces/IFiltrosDeEventos";
 
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
     default: []
+})
+
+export const filtroDeEventos = atom<IFiltrosDeEventos>({
+    key:'filtroDeEventos',
+    default:{}
 })
